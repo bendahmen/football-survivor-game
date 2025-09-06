ffd0b660b69f313f590192889dfcddcc34efb11d
Ben Dahmen <bendahmen@gmail.com>
2025-09-06 15:35:03 +0100

    change DB

--- football_survivor_game/settings_production.py
+++ football_survivor_game/settings_production.py
@@ -19,7 +19,6 @@ ALLOWED_HOSTS = [
 # Database - Render provides DATABASE_URL
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
         conn_max_age=600,
         conn_health_checks=True,
     )
